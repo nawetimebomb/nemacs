0caa79ec2b6fd6e1fd78b03ca82df42f3c7965d8
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2017-06-15 09:50:56 -0300

    Add Java and Scala

--- init.el
+++ init.el
@@ -8,6 +8,7 @@
   ;; Install dependencies
   (require 'package)
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+  (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   (package-initialize)
 
