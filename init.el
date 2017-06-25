2fb6c903273a88f753c14bc10c86286642f2126e
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2017-06-25 20:40:59 -0300

    Remove Scala and Java, Major fixes

--- init.el
+++ init.el
@@ -8,7 +8,6 @@
   ;; Install dependencies
   (require 'package)
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-  (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   (package-initialize)
 
