8d25fd3b08e335840ca98bba7227f92e444b8799
Nahuel Jesús Sacchetti <nahueljsacchetti@gmail.com>
2018-12-18 06:34:46 -0600

    Change gnus with gnus-no-server

--- lisp/nemacs-global-keybindings.el
+++ lisp/nemacs-global-keybindings.el
@@ -31,6 +31,6 @@
 (global-set-key (kbd "C-z g") #'magit-status)
 
 ;; mu4e
-(global-set-key (kbd "C-z m") #'gnus)
+(global-set-key (kbd "C-z m") #'gnus-no-server)
 
 (provide 'nemacs-global-keybindings)
