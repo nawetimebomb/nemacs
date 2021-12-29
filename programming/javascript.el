2df80904096546311ee68314532c1295b86c25cf
Nahuel Jesus Sacchetti <me@nsacchetti.com>
2021-12-28 22:40:01 -0600

    programming: JavaScript style on error change

--- programming/javascript.el
+++ programming/javascript.el
@@ -4,4 +4,6 @@
   (rjsx-mode . nemacs-setup-default-prog-mode)
   :custom
   (js-indent-level 4)
-  (sgml-basic-offset 4))
+  (sgml-basic-offset 4)
+  :custom-face
+  (js2-error ((t :underline (:style wave :color "#3e9688")))))
