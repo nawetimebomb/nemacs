cb33b8b8bd62f99aff6b69a40c1b695043a959a1
Nahuel Jesus Sacchetti <me@nsacchetti.com>
2021-12-28 21:53:22 -0600

    modules/interface: Change theme to base16-apathy. Add doom-modeline.

--- modules/interface.el
+++ modules/interface.el
@@ -1,7 +1,23 @@
-(use-package night-owl-theme
+(use-package base16-theme
   :init
-  (load-theme 'night-owl t))
+  (load-theme 'base16-apathy t)
+  :custom-face
+  (fringe ((t :background nil)))
+  (mode-line-inactive ((t (:background "#0a312a" :foreground "#2b685e"))))
+  (success ((t :foreground "#b266c0" :weight bold))))
 
-(use-package nano-modeline
+(use-package doom-modeline
   :init
-  (nano-modeline-mode))
+  (doom-modeline-mode)
+  :custom
+  (doom-modeline-bar-width 2)
+  (doom-modeline-buffer-encoding nil)
+  (doom-modeline-buffer-file-name-style 'buffer-name)
+  (doom-modeline-buffer-state-icon nil)
+  (doom-modeline-height 35)
+  (doom-modeline-major-mode-color-icon nil)
+  (doom-modeline-minor-modes nil)
+  :custom-face
+  (doom-modeline-bar ((t :background nil)))
+  (doom-modeline-buffer-file ((t :inherit 'doom-modeline-info)))
+  (doom-modeline-buffer-modified ((t :slant italic))))
