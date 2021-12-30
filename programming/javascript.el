db38605388383adf154230cb920efc0ba3c19e22
Nahuel Jesus Sacchetti <me@nsacchetti.com>
2021-12-29 19:17:12 -0600

    programming: JavaScript changes

--- programming/javascript.el
+++ programming/javascript.el
@@ -1,9 +1,13 @@
 (use-package rjsx-mode
+  :preface
+  (defun nemacs-setup-rjsx-mode ()
+    (flycheck-mode))
   :mode "\\.js\\'"
   :hook
-  (rjsx-mode . nemacs-setup-default-prog-mode)
+  (rjsx-mode . nemacs-setup-rjsx-mode)
   :custom
   (js-indent-level 4)
   (sgml-basic-offset 4)
   :custom-face
+  (rjsx-text ((t :inherit default)))
   (js2-error ((t :underline (:style wave :color "#3e9688")))))
