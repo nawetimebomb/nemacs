e0999b07995aa3acb37f82ceffdb33ce1858ae1e
Nahuel J. Sacchetti <me@nsacchetti.com>
2021-12-30 00:20:25 -0600

    general: Remove __OLD__ code

--- __OLD__/theme.el
+++
@@ -1,6 +0,0 @@
-(add-to-list 'custom-theme-load-path (concat user-emacs-directory "__OLD__/themes/"))
-
-(set-fontset-font t 'unicode (font-spec :name "Envy Code R-14") nil)
-(set-face-font 'default "Envy Code R-14")
-
-(load-theme 'monochrome-dark t)
