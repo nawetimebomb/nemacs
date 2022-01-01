32f94e68fa6b574587ce1cc24387a4fc0ea7541a
Nahuel Jesus Sacchetti <me@nsacchetti.com>
2022-01-01 02:15:45 -0600

    modules: Reorder prog.el file packages

--- modules/prog.el
+++ modules/prog.el
@@ -2,10 +2,10 @@
   :custom
   (flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
 
-(use-package lsp-mode)
-
 (use-package log-edit
   :custom
   (log-edit-confirm 'changed)
   (log-edit-keep-buffer nil)
   (log-edit-require-final-newline t))
+
+(use-package lsp-mode)
