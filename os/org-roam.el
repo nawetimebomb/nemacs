318dfd9fa2e3915d8475f816920e98a0b6e0077e
Nahuel Jesus Sacchetti <me@nsacchetti.com>
2022-01-01 23:15:34 -0600

    os: Migration from exwm. Add org-roam

---
+++ os/org-roam.el
@@ -0,0 +1,19 @@
+(use-package org-roam
+  :bind
+  ((:map org-mode-map
+         ("C-M-i" . completion-at-point))
+   ("C-c n l" . org-roam-buffer-toggle)
+   ("C-c n f" . org-roam-node-find)
+   ("C-c n i" . org-roam-node-insert)
+   (:map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-today)))
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
+  :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-db-autosync-mode)
+  (org-roam-setup)
+  :custom
+  (org-roam-directory "~/Notes/Roam")
+  (org-roam-dailies-directory "Journal/"))
