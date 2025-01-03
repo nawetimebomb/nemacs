59c24dddf8fe00d662f981fb9f1beec503734ad5
Nahuel J. Sacchetti <me@nsacchetti.com>
2025-01-03 12:50:02 -0600

    feat: add v programming language

--- programming.el
+++ programming.el
@@ -57,3 +57,16 @@
  'prog-mode-hook #'(lambda ()
                      (company-mode)
                      (display-line-numbers-mode)))
+
+(use-package v-mode
+  :straight (v-mode
+             :type git
+             :host github
+             :repo "damon-kwok/v-mode"
+             :files ("tokens" "v-mode.el"))
+  :config
+  :bind-keymap
+  ("M-z" . v-menu)
+  ("<f6>" . v-menu)
+  ("C-c C-f" . v-format-buffer)
+  :mode ("\\(\\.v?v\\|\\.vsh\\)$" . 'v-mode))
